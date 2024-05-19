49396432461fa887d59c8b8c4e25995373bda9f5
Mario Fuchs <mariofuchs.w@gmail.com>
2024-05-19 11:54:53 +0200

    Provide reporting for all partitions (#6)

--- scripts/image_analyzer.py
+++ scripts/image_analyzer.py
@@ -13,14 +13,31 @@ def parse_arguments():
     return args.image_path, args.output_csv
 
 
+def safe_decode(byte_string):
+    try:
+        return byte_string.decode('utf-8')
+    except UnicodeDecodeError:
+        return byte_string.decode('latin-1', errors='ignore')
+
+
 def list_directories(filesystem_object, directory_path="/"):
     file_metadata_list = []
-    directory = filesystem_object.open_dir(directory_path)
+    try:
+        directory = filesystem_object.open_dir(directory_path)
+    except OSError as e:
+        print(f"Error opening directory {directory_path}: {e}")
+        return file_metadata_list
+
     for entry in directory:
-        if entry.info.name.name.decode() not in [".", ".."]:
-            full_path = os.path.join(directory_path, entry.info.name.name.decode())
+        entry_name = safe_decode(entry.info.name.name)
+        if entry_name not in [".", ".."]:
+            full_path = os.path.join(directory_path, entry_name)
+            #print(full_path)
             if entry.info.meta and entry.info.meta.type == pytsk3.TSK_FS_META_TYPE_DIR:
-                file_metadata_list.extend(list_directories(filesystem_object, full_path))
+                try:
+                    file_metadata_list.extend(list_directories(filesystem_object, full_path))
+                except OSError as e:
+                    print(f"Error accessing subdirectory {full_path}: {e}")
             else:
                 try:
                     if entry.info.meta:
@@ -29,15 +46,15 @@ def list_directories(filesystem_object, directory_path="/"):
                         file_metadata = {
                             'Path': full_path,
                             'Inode': meta_info.addr,
-                            'Name': file_object.info.name.name.decode(),
+                            'Name': entry_name,
                             'Size': meta_info.size,
-                            'Creation Time': datetime.datetime.fromtimestamp(meta_info.crtime).strftime('%Y-%m-%d %H:%M:%S')
+                            'Creation Time': datetime.datetime.fromtimestamp(meta_info.crtime).strftime(
+                                '%Y-%m-%d %H:%M:%S')
                         }
                         file_metadata_list.append(file_metadata)
                 except Exception as e:
-                    print(f"Error accessing file metadata: {e}")
+                    print(f"Error accessing file metadata for {full_path}: {e}")
     return file_metadata_list
-
 
 
 def get_partition_info(imagehandle):
@@ -53,16 +70,15 @@ def get_partition_info(imagehandle):
                 'Flag': partition.flags
             }
             partition_info_list.append(partition_info)
-            partition_start = partition.start
-            #print("Hello",partition_start)
-            #filesystemObject = pytsk3.FS_Info(imagehandle, offset=partition_start * 512)
-            #list_directories(filesystemObject)
         except Exception as e:
             print(f"Error processing partition: {e}")
     return partition_info_list
 
 
 ########################################################################################################################
+################################################### REPORTING ##########################################################
+########################################################################################################################
+
 def save_partition_info_to_csv(partition_info, output_csv):
     file_exists = os.path.isfile(output_csv)
     with open(output_csv + 'partitionInfo.csv', 'w+', newline='') as csv_file:
@@ -74,22 +90,17 @@ def save_partition_info_to_csv(partition_info, output_csv):
     print(f"Partition info saved to {output_csv}")
 
 
-def save_file_metadata_to_csv(file_metadata, output_csv):
+def save_file_metadata_to_csv(partition, file_metadata, output_csv):
     file_exists = os.path.isfile(output_csv)
-    with open(output_csv + 'fileMetadata.csv', 'w+', newline='') as csv_file:
+    with open(output_csv + 'fileMetadata-Partition' + str(partition) + '.csv', 'w+', newline='') as csv_file:
         writer = csv.writer(csv_file)
         if not file_exists:
             writer.writerow(['Path', 'Inode', 'Name', 'Size', 'Creation Time'])
         for metadata in file_metadata:
-            writer.writerow([metadata['Path'], metadata['Inode'], metadata['Name'], metadata['Size'], metadata['Creation Time']])
-    print(f"File metadata saved to {output_csv}")
+            writer.writerow(
+                [metadata['Path'], metadata['Inode'], metadata['Name'], metadata['Size'], metadata['Creation Time']])
+    print(f"File metadata of partition {str(partition)} saved to {output_csv}")
 
-
-'''
-TO-DO:
-* create meta data csv file for each partition and name those files accordingly
- 
-'''
 
 if __name__ == "__main__":
     image_path, output_csv = parse_arguments()
@@ -98,5 +109,10 @@ if __name__ == "__main__":
     partition_info = get_partition_info(imagehandle)
     save_partition_info_to_csv(partition_info, output_csv)
 
-    file_metadata = list_directories(pytsk3.FS_Info(imagehandle, offset=32 * 512))
-    save_file_metadata_to_csv(file_metadata, output_csv)
+    for i in range(len(partition_info)):
+        try:
+            file_metadata = list_directories(pytsk3.FS_Info(imagehandle, offset=partition_info[i]['Start'] * 512))
+            save_file_metadata_to_csv(partition_info[i]['Partition'], file_metadata, output_csv)
+        except OSError as e:
+            print(f"Cannot access partition {partition_info[i]['Partition']}")
+            continue
